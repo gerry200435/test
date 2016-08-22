f5029d33d7c19dc423c57d441a808a42b236d875
Gerry Yan <gerry@GerrydeMacBook-Pro.local>
2016-08-22 22:42:49 +0800

    test

--- conf/db.js
+++ conf/db.js
@@ -1,14 +1,28 @@
 /**
  * Created by gerry on 16/8/5.
  */
+var sequelize = require('sequelize');
+
 // conf/db.js
 // MySQL数据库联接配置
-module.exports = {
+var dbconf = {
 	mysql: {
 		host: 'aliyun.invalo.com',
 		user: 'nodetest',
 		password: 'Nodetest123',
-		database:'node_test',
+		database: 'node_test',
 		port: 3306
 	}
 };
+
+var sequelize = new sequelize(dbconf.mysql.database, dbconf.mysql.user, dbconf.mysql.password, {
+	host: dbconf.mysql.host,
+	dialect: 'mysql',
+	pool: {
+		max: 5,
+		min: 0,
+		idle: 10000
+	}
+});
+
+module.exports = sequelize;
--- routes/msg.js
+++ routes/msg.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var sql = require('../conf/db');
 
 /* GET msg list. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
 	res.end('Hello');
+});
+
+router.get('/add', function (req, res, next) {
+	res.send('Hello add');
+	next();
+});
+
+router.get('/test', function (req, res, next) {
+	sql.authenticate()
+		.then(function (err) {
+			res.send('Connection has been established successfully.');
+		})
+		.catch(function (err) {
+			res.send('err');
+			console.log('Unable to connect to the database:', err);
+		});
+	next();
 });
 
 module.exports = router;
